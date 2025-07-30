410cb791ee5c95faee91fa1e16d73375b68189a5
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-07-30 16:34:16 +0700

    feat: add AoE skill

--- Assets/Scripts/Skill/AoESkill.cs
+++ Assets/Scripts/Skill/AoESkill.cs
@@ -1,29 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Skills/AoE Skill")]
+[CreateAssetMenu(menuName = "Skills/Shield Skill")]
 public class AoESkill : Skill
 {
-    public float radius = 3f;
-    public int damage = 10;
-    public LayerMask enemyLayer;
-    public GameObject effectPrefab;
+    public GameObject _prefab;
 
     public override void UseSkill(GameObject user)
     {
-        // Collider2D[] enemies = Physics2D.OverlapCircleAll(user.transform.position, radius, enemyLayer);
-
-        // foreach (Collider2D enemy in enemies)
-        // {
-        //     var health = enemy.GetComponent<EnemyHealth>();
-        //     if (health != null)
-        //     {
-        //         health.TakeDamage(damage);
-        //     }
-        // }
-
-        // if (effectPrefab != null)
-        // {
-        //     Object.Instantiate(effectPrefab, user.transform.position, Quaternion.identity);
-        // }
+        Instantiate(_prefab, user.transform.position, Quaternion.identity, user.transform);
     }
 }
